0ca5d2c4091f23e20b1fb803d3a46897b3d9ce18
Matias Näveri <madeingarage@hotmail.com>
2026-03-15 22:17:07 +0200

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics";
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+inject();
+createRoot(document.getElementById("root")!).render(<App />);
